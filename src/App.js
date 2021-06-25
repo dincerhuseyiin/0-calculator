a6a6ec5432048843b7b1554b81f4247915eb9ebe
dincer <80000587+dincerhuseyiin@users.noreply.github.com>
2021-06-25 14:50:18 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import React, { useState } from "react";
 
+
 function App() {
-  const [result, setResult] = useState("");
+  const [result, setResult] = useState("0");
   const [list, setList] = useState([]);
   const [histBut, setHistButton] = useState(false);
 
   const handleClick = (e) => {
-    setResult(result + e.target.name);
+    setResult( result + e.target.name);
   };
 
   const clearAll = () => {
@@ -21,6 +22,7 @@ function App() {
   };
 
   const calculate = () => {
+ 
     const detail = {
       id: Math.floor(Math.random() * 1000),
       name: "detail",
@@ -32,11 +34,12 @@ function App() {
     } catch (err) {
       setResult("Error!");
     }
+    
   };
 
   const currDate = new Date().toLocaleDateString();
   const currTime = new Date().toLocaleTimeString();
-
+  
   return (
     <>
       <div className="container">
@@ -44,29 +47,29 @@ function App() {
           <input type="text" value={result} />
         </form>
 
-        <button className="history" onClick={() => setHistButton(!histBut)}>
+        <button className="btn" className="history" onClick={() => setHistButton(!histBut)}>
           {" "}
           HISTORY{" "}
         </button>
         {histBut ? (
-          <div className="container2">
+          <div className="container2" id="scroller">
             <p>My history</p>
             <button className="clean-hist" onClick={() => setList([])}>
               Clean All History
             </button>
             {list !== [] ? (
               <ul>
-                {list.map((t, i) => (
-                  <li>
-                    {" "}
-                    {currDate} <br /> {currTime} <br /> {t.value}
+                {list.map((t, index) => (
+                  <li key={index}>
+                    
+                    {currDate} {currTime} <br />  {t.value} = {eval(t.value).toString()}
                   </li>
                 ))}
               </ul>
             ) : null}
           </div>
         ) : null}
-
+        
         <div className="keypad">
           <button
             className="midgrey"
@@ -87,57 +90,59 @@ function App() {
           <button className="midgrey" name="%" onClick={rate}>
             %
           </button>
-          <button name="/" onClick={handleClick} id="operators">
+          <button className="btn" name="/" onClick={handleClick} id="operators">
             &divide;
           </button>
-          <button name="7" onClick={handleClick}>
+          <button className="btn" name="7" onClick={handleClick}>
             7
           </button>
-          <button name="8" onClick={handleClick}>
+          <button className="btn" name="8" onClick={handleClick}>
             8
           </button>
-          <button name="9" onClick={handleClick}>
+          <button className="btn" name="9" onClick={handleClick}>
             9
           </button>
-          <button name="*" onClick={handleClick} id="operators">
+          <button className="btn" name="*" onClick={handleClick} id="operators">
             &times;
           </button>
-          <button name="4" onClick={handleClick}>
+          <button className="btn" name="4" onClick={handleClick}>
             4
           </button>
-          <button name="5" onClick={handleClick}>
+          <button className="btn" name="5" onClick={handleClick}>
             5
           </button>
-          <button name="6" onClick={handleClick}>
+          <button className="btn" name="6" onClick={handleClick}>
             6
           </button>
-          <button name="-" onClick={handleClick} id="operators">
+          <button className="btn" name="-" onClick={handleClick} id="operators">
             &ndash;
           </button>
-          <button name="1" onClick={handleClick}>
+          <button className="btn" name="1" onClick={handleClick}>
             1
           </button>
-          <button name="2" onClick={handleClick}>
+          <button className="btn" name="2" onClick={handleClick}>
             2
           </button>
-          <button name="3" onClick={handleClick}>
+          <button className="btn" name="3" onClick={handleClick}>
             3
           </button>
-          <button name="+" onClick={handleClick} id="operators">
+          <button className="btn" name="+" onClick={handleClick} id="operators">
             +
           </button>
-          <button name="0" onClick={handleClick} id="zero">
+          <button className="btn" name="0" onClick={handleClick} id="zero">
             0
           </button>
-          <button name="." onClick={handleClick}>
+          <button className="btn" name="." onClick={handleClick}>
             ,
           </button>
           <button name="=" onClick={calculate} id="operators">
             =
           </button>
+          
         </div>
       </div>
     </>
   );
 }
+
 export default App;
